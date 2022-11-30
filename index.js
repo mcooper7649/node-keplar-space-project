7ef82a2ffb7a4ffbcf03fc7434af31b4217c54c9
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-30 10:33:15 -0500

    read and parsed data, then created a function that conditionally checked for habitable planets and logged habitable planets

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
-const results = [];
-// Create the parser
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
 
 fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on('error', (err) => {
     console.error(err);
   })
   .on('end', () => {
-    console.log(results);
-    console.log('done');
+    console.log(habitablePlanets);
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
 
 //parse
